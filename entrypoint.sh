4e67e687ac89c202966526f925eb4cc7f87de224
RNTechnologyCanada <r.n.technology.canada@gmail.com>
2026-02-06 14:07:58 -0400

    changed sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
-# Use env vars for DB connection
-odoo -c /etc/odoo/odoo.conf --db_host=$POSTGRES_HOST --db_port=$POSTGRES_PORT \
---db_user=$POSTGRES_USER --db_password=$POSTGRES_PASSWORD --workers=0
+echo "Render PORT is: $PORT"
+echo "Starting Odoo..."
+
+exec odoo \
+  --config=/etc/odoo/odoo.conf \
+  --db_host="$ODOO_DB_HOST" \
+  --db_port="$ODOO_DB_PORT" \
+  --db_user="$ODOO_DB_USER" \
+  --db_password="$ODOO_DB_PASSWORD" \
+  --db_name="$ODOO_DB_NAME" \
+  --xmlrpc_port="$PORT"
