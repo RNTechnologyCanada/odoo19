4f007aa9d60b4e189234243b4f9abae5cf65bfe2
RNTechnologyCanada <r.n.technology.canada@gmail.com>
2026-02-06 14:35:49 -0400

    odoo error

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,10 @@
 set -e
 
 echo "Render PORT is: $PORT"
-echo "Starting Odoo..."
+echo "Initializing Odoo database (base module)..."
 
 exec odoo \
   --config=/etc/odoo/odoo.conf \
-  --db_host="$ODOO_DB_HOST" \
-  --db_port="$ODOO_DB_PORT" \
-  --db_user="$ODOO_DB_USER" \
-  --db_password="$ODOO_DB_PASSWORD" \
+  -i base \
+  --without-demo=all \
   --http-port="$PORT"
